0bbf773155740616f07f0aec10014c9bd5e3292e
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2019-06-08 19:37:39 -0300

    DesgasteSegunMultiplicador posee una durabilidad.

--- src/DesgasteSegunMultiplicador.java
+++ src/DesgasteSegunMultiplicador.java
@@ -1,14 +1,20 @@
-import Materiales.Material;
-
 public class DesgasteSegunMultiplicador {
 
     private int multiplicador;
+    private int durabilidad;
 
-    public DesgasteSegunMultiplicador(int multiplicadorInicial){
-        this.multiplicador = multiplicadorInicial;
+    public DesgasteSegunMultiplicador(int multiplicadorDesgaste, int durabilidadDesgaste) {
+        this.multiplicador = multiplicadorDesgaste;
+        this.durabilidad = durabilidadDesgaste;
     }
 
-    public void desgastarCon(int cantidad, Material material){
-        material.reducirDurabilidadEn(cantidad);
+    public void desgastarCon(int cantidad){
+
+        this.durabilidad = this.durabilidad - cantidad * this.multiplicador;
+
+    }
+
+    public int durabilidad(){
+        return this.durabilidad;
     }
 }
