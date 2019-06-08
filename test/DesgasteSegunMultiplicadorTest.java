7e0d305bda37255dd7cc6198b81cfafab6b7c82b
aldy <aldanuchi@gmail.com>
2019-06-08 19:29:26 -0300

    DesgasteSegunMultiplicador posee una durabilidad.

--- test/DesgasteSegunMultiplicadorTest.java
+++ test/DesgasteSegunMultiplicadorTest.java
@@ -1,21 +1,41 @@
-import Materiales.Madera;
-import Materiales.Material;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class DesgasteSegunMultiplicadorTest {
     @Test
-    public void testDesgasteSegunMultiplicadorDesgastaAlMaterialRecibidoEnLaCantidadQueSeLePasaMultiplicadaPorElMultiplicadorConQueSeCrea(){
+    public void testDesgasteSegunMultiplicadorSeDesgastaEnLaCantidadRecibidaMultiplicadaPorElMultiplicadorConQueSeCrea(){
 
-        Material material = new Madera(50);
-        int durabilidadInicial = material.durabilidad();
+        int durabilidadInicial = 10;
+        DesgasteSegunMultiplicador desgaste = new DesgasteSegunMultiplicador(1,durabilidadInicial);
+        desgaste.desgastarCon(1);
 
-        DesgasteSegunMultiplicador desgaste = new DesgasteSegunMultiplicador(1);
-        desgaste.desgastarCon(1,material);
-
-        assertEquals(durabilidadInicial-1,material.durabilidad());
+        assertEquals(durabilidadInicial-1,desgaste.durabilidad());
 
     }
+
+    @Test
+    public void test2DesgasteSegunMultiplicadorSeDesgastaEnLaCantidadRecibidaMultiplicadaPorElMultiplicadorConQueSeCrea(){
+
+        int durabilidadInicial = 10;
+        DesgasteSegunMultiplicador desgaste = new DesgasteSegunMultiplicador(1,durabilidadInicial);
+        desgaste.desgastarCon(2);
+
+        assertEquals(durabilidadInicial-2,desgaste.durabilidad());
+
+    }
+
+    @Test
+    public void test3DesgasteSegunMultiplicadorSeDesgastaEnLaCantidadRecibidaMultiplicadaPorElMultiplicadorConQueSeCrea(){
+
+        int durabilidadInicial = 10;
+        DesgasteSegunMultiplicador desgaste = new DesgasteSegunMultiplicador(2,durabilidadInicial);
+        desgaste.desgastarCon(1);
+
+        assertEquals(durabilidadInicial-2,desgaste.durabilidad());
+
+    }
+
+
 
 }
