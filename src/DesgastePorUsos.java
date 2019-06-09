a139a08224bbf3a3186510133b77bec778299999
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2019-06-08 22:19:08 -0300

    Se crea DesgastePorUsos.

---
+++ src/DesgastePorUsos.java
@@ -0,0 +1,24 @@
+
+public class DesgastePorUsos {
+    private int durabilidad;
+    private int usos;
+
+
+    public DesgastePorUsos(int seRompeAlUso) {
+        this.durabilidad = seRompeAlUso;
+        this.usos=0;
+
+    }
+
+    public void desgastar(){
+        this.usos = this.usos + 1;
+        if (this.usos == this.durabilidad){
+            this.durabilidad = 0;
+        }
+    }
+
+    public int durabilidad(){
+        return this.durabilidad;
+    }
+
+}
