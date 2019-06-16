c0708e4589e79ba2307999608e01e25421bc9906
mdabat <martin.dabat@gmail.com>
2019-06-16 15:38:37 -0300

    Commit al master, para ver si puedo mergear a master Todo lo del branch de mapa

--- src/Jugador/Jugador.java
+++ src/Jugador/Jugador.java
@@ -1,22 +1,23 @@
 package Jugador;
 
 import Herramientas.*;
-import Mapa.Casillero;
-import Mapa.Item;
+import Mapa.*;
 import TipoMaterial.*;
 import Recursos.*;
 
+
 public class Jugador implements Item {
 	private Inventario inventario;
-	private Herramienta herramientaDeTrabajo;
+	private Hacha herramientaDeTrabajo;
 	private Casillero casillero;
 
 	public Jugador() {
 		TipoMadera madera = new TipoMadera();
 		Hacha hacha = new Hacha(madera);
 		inventario = new Inventario(hacha);
-		
+
 		herramientaDeTrabajo = hacha;
+		this.setCasillero(new Casillero());
 	}
 
 
@@ -24,51 +25,38 @@ public class Jugador implements Item {
 	public void setCasillero(Casillero casillero){
 		this.casillero= casillero;
 	}
+	public void cambiarCasillero(Casillero casilleroNuevo){
+		this.casillero.setEstado(new EstadoDisponible());
+		this.setCasillero(casilleroNuevo);
+	}
 	public Casillero getCasillero(){
 		return this.casillero;
 	}
-	public int getFila(){
-		return this.casillero.getFila();
-	}
-	public int getColumna(){
-		return this.casillero.getColumna();
+	public Posicion getPosicion(){return this.casillero.getPosicion();}
+
+	public void mover(Mapa mapa, Direccion direccion){
+		this.casillero.getCasilleroVecino(mapa, direccion).setItem(this);
 	}
 
-	public void intercambiarCasilleros(Item item){
-		Casillero casillero= item.getCasillero();
-		item.setCasillero(this.getCasillero());
-		this.setCasillero(casillero);
-	}
-
-	public void moverArriba(){
-		this.casillero.moverArriba();
-	}
-	public void moverAbajo(){
-		this.casillero.moverAbajo();
-	}
-	public void moverIzquierda(){
-		this.casillero.moverIzquierda();
-	}
-	public void moverDerecha(){
-		this.casillero.moverDerecha();
-	}
-
+	public void ingresar(Mapa mapa, Posicion posicion){ mapa.getCasillero(posicion).setItem(this);}
 	// Fin implementacion
+
+
 
 	public void construirHacha(TipoMaterial unMaterial) {
 		Hacha hacha = new Hacha(unMaterial);
-		
+
 		inventario.agregarHerramienta(hacha);
 	}
-	
+
 	public void usarHerramienta(Recurso unRecurso) {
 		herramientaDeTrabajo.golpear(unRecurso);
 	}
-	
+
 	public Herramienta herraminentaActual() {
 		return herramientaDeTrabajo;
 	}
-	
+
 	public int cantidadHerramientas() {
 		return inventario.cantidadHerramientas();
 	}
