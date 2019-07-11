315879547f6566207ed3184f21722785ac6c0dbf
Minera José Luis <joseluis_minera@hotmail.com>
2019-07-11 01:41:45 -0300

    se añade contenedor de apertura para la pantalla de inicio del juego. Se sube su evento asociado al botòn comenzar

---
+++ src/Vista/ContenedorApertura.java
@@ -0,0 +1,53 @@
+package Vista;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import Eventos.BotonComenzarEventHandler;
+
+public class ContenedorApertura extends VBox {
+
+    Stage stage;
+
+    public ContenedorApertura(Stage stage, Scene proximaEscena) {
+
+        super();
+
+        this.stage = stage;
+
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(20);
+        this.setPadding(new Insets(25));
+        Image imagen = new Image("");
+        BackgroundImage imagenDeFondo = new BackgroundImage(imagen, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        this.setBackground(new Background(imagenDeFondo));
+
+        Button botonComenzar = new Button();
+        botonComenzar.setText("Entrar");
+
+        Label etiqueta = new Label();
+        etiqueta.setFont(Font.font("Tahoma", FontWeight.BOLD, 18));
+
+        etiqueta.setText("Bienvenidos a la aplicación ALgoCraft de algoritmos y programación III. Hacer click en comenzar");
+        etiqueta.setTextFill(Color.web("#66A7C5"));
+
+        BotonComenzarEventHandler botonComenzarHandler = new BotonComenzarEventHandler(stage, proximaEscena);
+        botonComenzar.setOnAction(botonComenzarHandler);
+
+        this.getChildren().addAll(etiqueta, botonComenzar);
+    }
+
+}
