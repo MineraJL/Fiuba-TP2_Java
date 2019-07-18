8537e8ed76fc1d7ff21b5798104d278b183e2e0b
aldy <aldanuchi@gmail.com>
2019-07-18 20:19:47 -0300

    Agrego coordenadas a PosicionEnlazada

--- src/Modelo/Mapa/PosicionEnlazada.java
+++ src/Modelo/Mapa/PosicionEnlazada.java
@@ -1,24 +1,44 @@
 package Modelo.Mapa;
 
 public class PosicionEnlazada {
-    PosicionEnlazada arriba;
-    PosicionEnlazada abajo;
-    PosicionEnlazada derecha;
-    PosicionEnlazada izquierda;
+    private PosicionEnlazada arriba;
+    private PosicionEnlazada abajo;
+    private PosicionEnlazada derecha;
+    private PosicionEnlazada izquierda;
 
-    public PosicionEnlazada(){
+    private int coorX;
+    private int coorY;
+
+    public PosicionEnlazada(int coorX, int coorY) {
+        this.coorX = coorX;
+        this.coorY = coorY;
+        this.inicializarSiguientes();
+    }
+
+    public int getX() {
+        return this.coorX;
+    }
+    public int getY() {
+        return this.coorY;
+    }
+
+    public boolean equals(Posicion otraPosicion){
+        return ((this.getX()==otraPosicion.getX()) && (this.getY() == otraPosicion.getY()));
+    }
+
+
+    // enlaces
+    private void inicializarSiguientes(){
         this.arriba = this;
         this.abajo = this;
         this.derecha = this;
         this.izquierda = this;
     }
 
-    public void enlazarAIzquierda(PosicionEnlazada vecino)
-    {
+    public void enlazarAIzquierda(PosicionEnlazada vecino){
         this.izquierda = vecino;
         vecino.enlazarADerecha(this);
     }
-
 
     public void enlazarADerecha(PosicionEnlazada vecino)
     {
@@ -26,8 +46,7 @@ public class PosicionEnlazada {
     }
 
 
-    public void enlazarArriba(PosicionEnlazada vecino)
-    {
+    public void enlazarArriba(PosicionEnlazada vecino){
         this.arriba = vecino;
         vecino.enlazarAbajo(this);
     }
@@ -39,16 +58,14 @@ public class PosicionEnlazada {
     public PosicionEnlazada casilleroAIzquierda() {
         return this.izquierda;
     }
-
     public PosicionEnlazada casilleroADerecha() {
         return this.derecha;
     }
-
     public PosicionEnlazada casilleroArriba() {
         return this.arriba;
     }
-
     public PosicionEnlazada casilleroAbajo() {
         return this.abajo;
     }
+
 }
