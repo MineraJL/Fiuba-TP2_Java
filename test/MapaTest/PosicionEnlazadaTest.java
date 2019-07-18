6b9cafd676b31aaf80e4ff38dcd245add8ece139
aldy <aldanuchi@gmail.com>
2019-07-18 20:11:39 -0300

    Renombro tests y variables (ahora se llama PosicionEnlazada)

--- test/MapaTest/PosicionEnlazadaTest.java
+++ test/MapaTest/PosicionEnlazadaTest.java
@@ -7,48 +7,51 @@ import static junit.framework.TestCase.assertEquals;
 
 public class PosicionEnlazadaTest {
 
+    // A11 A12
+    // A21 A22
+
     @Test
-    public void testSeEnlazaCasilleroaIzquierdaYSegundokvsrkevbsvbvbvb(){
-        PosicionEnlazada casilleroA11 = new PosicionEnlazada();
-        PosicionEnlazada casilleroA12 = new PosicionEnlazada();
-        casilleroA12.enlazarAIzquierda(casilleroA11);
-        assertEquals(casilleroA11, casilleroA12.casilleroAIzquierda());
+    public void testSeEnlazaPosicionIzquierda(){
+        PosicionEnlazada posicionA11 = new PosicionEnlazada();
+        PosicionEnlazada posicionA12 = new PosicionEnlazada();
+        posicionA12.enlazarAIzquierda(posicionA11);
+        assertEquals(posicionA11, posicionA12.casilleroAIzquierda());
     }
 
     @Test
-    public void testCasilleroADerecha(){
-        PosicionEnlazada casilleroA11 = new PosicionEnlazada();
-        PosicionEnlazada casilleroA12 = new PosicionEnlazada();
-        casilleroA12.enlazarAIzquierda(casilleroA11);
-        assertEquals(casilleroA12, casilleroA11.casilleroADerecha());
+    public void testPosicionADerecha(){
+        PosicionEnlazada posicionA11 = new PosicionEnlazada();
+        PosicionEnlazada posicionA12 = new PosicionEnlazada();
+        posicionA12.enlazarAIzquierda(posicionA11);
+        assertEquals(posicionA12, posicionA11.casilleroADerecha());
     }
 
 
     @Test
-    public void testCasilleroAArriba(){
-        PosicionEnlazada casilleroA11 = new PosicionEnlazada();
-        PosicionEnlazada casilleroA21 = new PosicionEnlazada();
-        casilleroA21.enlazarArriba(casilleroA11);
-        assertEquals(casilleroA11, casilleroA21.casilleroArriba());
+    public void testPosicionArriba(){
+        PosicionEnlazada posicionA11 = new PosicionEnlazada();
+        PosicionEnlazada posicionA21 = new PosicionEnlazada();
+        posicionA21.enlazarArriba(posicionA11);
+        assertEquals(posicionA11, posicionA21.casilleroArriba());
     }
 
     @Test
-    public void testCasilleroAbajo(){
-        PosicionEnlazada casilleroA11 = new PosicionEnlazada();
-        PosicionEnlazada casilleroA21 = new PosicionEnlazada();
-        casilleroA21.enlazarArriba(casilleroA11);
-        assertEquals(casilleroA21, casilleroA11.casilleroAbajo());
+    public void testPosicionAbajo(){
+        PosicionEnlazada posicionA11 = new PosicionEnlazada();
+        PosicionEnlazada posicionA21 = new PosicionEnlazada();
+        posicionA21.enlazarArriba(posicionA11);
+        assertEquals(posicionA21, posicionA11.casilleroAbajo());
     }
 
     @Test
-    public void testCasilleroArribaEIzquierda(){
-        PosicionEnlazada casilleroA12 = new PosicionEnlazada();
-        PosicionEnlazada casilleroA21 = new PosicionEnlazada();
-        PosicionEnlazada casilleroA22 = new PosicionEnlazada();
-        casilleroA22.enlazarArriba(casilleroA12);
-        casilleroA22.enlazarAIzquierda(casilleroA21);
+    public void testPosicionArribaEIzquierda(){
+        PosicionEnlazada posicionA12 = new PosicionEnlazada();
+        PosicionEnlazada posicionA21 = new PosicionEnlazada();
+        PosicionEnlazada posicionA22 = new PosicionEnlazada();
+        posicionA22.enlazarArriba(posicionA12);
+        posicionA22.enlazarAIzquierda(posicionA21);
 
-        assertEquals(casilleroA12, casilleroA22.casilleroArriba());
-        assertEquals(casilleroA21, casilleroA22.casilleroAIzquierda());
+        assertEquals(posicionA12, posicionA22.casilleroArriba());
+        assertEquals(posicionA21, posicionA22.casilleroAIzquierda());
     }
 }
