5dacc14a127de69de5b9d6382bd4a9d84e3f3dee
aldy <aldanuchi@gmail.com>
2019-07-18 21:57:48 -0300

    Direccion es una interfaz (metodo siguiente)

--- src/Modelo/Mapa/Direccion.java
+++ src/Modelo/Mapa/Direccion.java
@@ -1,52 +1,8 @@
 package Modelo.Mapa;
 
-public class Direccion {
+public interface Direccion {
 
-    private static Direccion arriba;
-    private static Direccion abajo;
-    private static Direccion derecha;
-    private static Direccion izquierda;
+    // return posicionActual.posicionAIzquierda();
 
-    static {
-        arriba = new Direccion(0, 1);
-        abajo = new Direccion(0, -1);
-        derecha = new Direccion(1, 0);
-        izquierda = new Direccion(-1, 0);
-    }
-
-
-    private int coordenadaX;
-    private int coordenadaY;
-
-    private Direccion(int x, int y) {
-        this.coordenadaX = x;
-        this.coordenadaY = y;
-
-    }
-
-    public static Direccion arriba() {
-        return arriba;
-    }
-
-    public static Direccion abajo() {
-        return abajo;
-    }
-
-    public static Direccion derecha() {
-        return derecha;
-    }
-
-    public static Direccion izquierda() {
-        return izquierda;
-    }
-
-    public Posicion proximaPosicion(Posicion posicion) {
-        int x = posicion.getX() + this.coordenadaX;
-        int y = posicion.getY() + this.coordenadaY;
-
-        return new Posicion(x, y);
-    }
-
-
-
+    PosicionEnlazada siguiente(PosicionEnlazada posicionActual);
 }
