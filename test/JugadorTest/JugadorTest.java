27476c46252ed2b6eae941d1f7ade6d2b7ea88cf
aldy <aldanuchi@gmail.com>
2019-07-18 22:56:12 -0300

    Elimino tests mover para reemplararlos por otros (ya no se prueba así, jugador no puede moverse entre dos posiciones aisladas sino solamente en un mapa).

--- test/JugadorTest/JugadorTest.java
+++ test/JugadorTest/JugadorTest.java
@@ -32,23 +32,7 @@ public class JugadorTest {
 
         assertSame(mapa.getCasillero(posicionFinal),jugador.getCasillero());
     }
-	
-    @Test
-    public void testItemJugadorSeMueveALaDerechaEnElMapaDosVeces() {
-        Mapa mapa = new Mapa(10,10);
-        Casillero casillero1 = new Casillero();
-        Jugador jugador = new Jugador();
-        jugador.setCasillero(casillero1);
-        PosicionEnlazada posicionInicial = new PosicionEnlazada(5,5);
-        jugador.ingresar(mapa, posicionInicial);
-        // Lo muevo a la derecha 2 veces
-        jugador.mover(mapa, Direccion.derecha());
-        jugador.mover(mapa, Direccion.derecha());
-        PosicionEnlazada posicionFinal = new PosicionEnlazada(7,5);
 
-        assertSame(mapa.getCasillero(posicionFinal),jugador.getCasillero());
-    }
-	
 	/*@Test
 	public void testJugadorConstruyeHachaDeMadera() {
 		TipoMadera unaMadera = new TipoMadera();
