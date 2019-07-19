b4b88807d1d2cabec6f37ab7d96bb004eaf7a614
aldy <aldanuchi@gmail.com>
2019-07-18 21:56:21 -0300

    Posicion -> PosicionEnlazada en tests.

--- test/MapaTest/PosicionEnlazadaTest.java
+++ test/MapaTest/PosicionEnlazadaTest.java
@@ -15,7 +15,7 @@ public class PosicionEnlazadaTest {
         PosicionEnlazada posicionA11 = new PosicionEnlazada(1,1);
         PosicionEnlazada posicionA12 = new PosicionEnlazada(1,2);
         posicionA12.enlazarAIzquierda(posicionA11);
-        assertEquals(posicionA11, posicionA12.casilleroAIzquierda());
+        assertEquals(posicionA11, posicionA12.posicionAIzquierda());
     }
 
     @Test
@@ -23,7 +23,7 @@ public class PosicionEnlazadaTest {
         PosicionEnlazada posicionA11 = new PosicionEnlazada(1,1);
         PosicionEnlazada posicionA12 = new PosicionEnlazada(1,2);
         posicionA12.enlazarAIzquierda(posicionA11);
-        assertEquals(posicionA12, posicionA11.casilleroADerecha());
+        assertEquals(posicionA12, posicionA11.posicionADerecha());
     }
 
 
@@ -32,7 +32,7 @@ public class PosicionEnlazadaTest {
         PosicionEnlazada posicionA11 = new PosicionEnlazada(1,1);
         PosicionEnlazada posicionA21 = new PosicionEnlazada(2,1);
         posicionA21.enlazarArriba(posicionA11);
-        assertEquals(posicionA11, posicionA21.casilleroArriba());
+        assertEquals(posicionA11, posicionA21.posicionArriba());
     }
 
     @Test
@@ -40,7 +40,7 @@ public class PosicionEnlazadaTest {
         PosicionEnlazada posicionA11 = new PosicionEnlazada(1,1);
         PosicionEnlazada posicionA21 = new PosicionEnlazada(2,1);
         posicionA21.enlazarArriba(posicionA11);
-        assertEquals(posicionA21, posicionA11.casilleroAbajo());
+        assertEquals(posicionA21, posicionA11.posicionAbajo());
     }
 
     @Test
@@ -51,7 +51,7 @@ public class PosicionEnlazadaTest {
         posicionA22.enlazarArriba(posicionA12);
         posicionA22.enlazarAIzquierda(posicionA21);
 
-        assertEquals(posicionA12, posicionA22.casilleroArriba());
-        assertEquals(posicionA21, posicionA22.casilleroAIzquierda());
+        assertEquals(posicionA12, posicionA22.posicionArriba());
+        assertEquals(posicionA21, posicionA22.posicionAIzquierda());
     }
 }
