bca9334203995375e317fe60e24c63bc3fe3451e
Minera José Luis <joseluis_minera@hotmail.com>
2019-07-20 23:43:07 -0300

    Se trasladan test de movimiento del jugador de MapaIntegracionTest a JugadorTest, por considerar que son test propios del jugador.

--- test/JugadorTest/JugadorTest.java
+++ test/JugadorTest/JugadorTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertSame;
+import static junit.framework.TestCase.assertTrue;
 
 public class JugadorTest {
 	@Test
@@ -37,6 +38,75 @@ public class JugadorTest {
         PosicionEnlazada posicionFinal = new PosicionEnlazada(5,5);
 
         assertSame(mapa.getCasillero(posicionFinal),jugador.getCasillero());
+    }
+    
+    @Test
+    public void testJugadorSeMueveHaciaLaIzquierdaEnElMapaUnaVez() {
+        Mapa mapa = new Mapa(10,10);
+        PosicionEnlazada posicionInicial = new PosicionEnlazada(2,5);
+        Casillero casillero = new Casillero(posicionInicial);
+    	Inventario inventario = new Inventario();
+    	Mesa mesa = new Mesa();
+        Jugador jugador = new Jugador(inventario, mesa);
+        jugador.setCasillero(casillero);
+        jugador.ingresar(mapa, posicionInicial);
+
+        jugador.mover(new DireccionIzquierda());
+        PosicionEnlazada posicionFinal = new PosicionEnlazada(1,5);
+
+        assertTrue(jugador.getCasillero().getPosicion().equals(posicionFinal));
+    }
+
+    @Test
+    public void testOcupanteJugadorSeMueveHaciaLaDerechaEnElMapaUnaVez() {
+        Mapa mapa = new Mapa(10,10);
+        PosicionEnlazada posicionInicial = new PosicionEnlazada(2,5);
+        Casillero casillero = new Casillero(posicionInicial);
+    	Inventario inventario = new Inventario();
+    	Mesa mesa = new Mesa();
+        Jugador jugador = new Jugador(inventario, mesa);
+        jugador.setCasillero(casillero);
+        jugador.ingresar(mapa, posicionInicial);
+
+        jugador.mover(new DireccionDerecha());
+        PosicionEnlazada posicionFinal = new PosicionEnlazada(3,5);
+
+
+        assertTrue(jugador.getCasillero().getPosicion().equals(posicionFinal));
+    }
+
+    @Test
+    public void testJugadorSeMueveHaciaArribaEnElMapaUnaVez() {
+        Mapa mapa = new Mapa(10,10);
+        PosicionEnlazada posicionInicial = new PosicionEnlazada(2,5);
+        Casillero casillero = new Casillero(posicionInicial);
+    	Inventario inventario = new Inventario();
+    	Mesa mesa = new Mesa();
+        Jugador jugador = new Jugador(inventario, mesa);
+        jugador.setCasillero(casillero);
+        jugador.ingresar(mapa, posicionInicial);
+
+        jugador.mover(new DireccionArriba());
+        PosicionEnlazada posicionFinal = new PosicionEnlazada(2,6);
+
+        assertTrue(jugador.getCasillero().getPosicion().equals(posicionFinal));
+    }
+
+    @Test
+    public void testJugadorSeMueveHaciaAbajoEnElMapaUnaVez() {
+        Mapa mapa = new Mapa(10,10);
+        PosicionEnlazada posicionInicial = new PosicionEnlazada(2,5);
+        Casillero casillero = new Casillero(posicionInicial);
+    	Inventario inventario = new Inventario();
+    	Mesa mesa = new Mesa();
+        Jugador jugador = new Jugador(inventario, mesa);
+        jugador.setCasillero(casillero);
+        jugador.ingresar(mapa, posicionInicial);
+
+        jugador.mover(new DireccionAbajo());
+        PosicionEnlazada posicionFinal = new PosicionEnlazada(2,4);
+
+        assertTrue(jugador.getCasillero().getPosicion().equals(posicionFinal));
     }
 
 	/*@Test
