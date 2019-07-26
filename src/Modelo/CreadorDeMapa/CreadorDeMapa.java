b8e9888927023eb84c226e2a0d364d1d279b7dd5
aldy <aldanuchi@gmail.com>
2019-07-26 17:46:39 -0300

    Arreglo inicialización de Mapa con Recursos.

--- src/Modelo/CreadorDeMapa/CreadorDeMapa.java
+++ src/Modelo/CreadorDeMapa/CreadorDeMapa.java
@@ -18,6 +18,11 @@ public class CreadorDeMapa {
     }
 
     private void inicializarMapaConRecursos(){
+        this.inicializarMapaConRecursosNecesariosParaJugar();
+        this.agregarMaderasAdicionales();
+    }
+
+    private void inicializarMapaConRecursosNecesariosParaJugar(){
         this.inicializarMapaConMadera();
         this.inicializarMapaConPiedra();
         this.inicializarMapaConMetal();
@@ -31,26 +36,26 @@ public class CreadorDeMapa {
 
     private void inicializarMapaConMadera(){
         // ubico 20 maderas en el mapa
-        Madera madera = new Madera();
-        for (int i=3;i<8;i++) {
+        for (int i=3;i<7;i++) {
             for (int j = 3; j < 8; j++) {
                 PosicionEnlazada posicion = new PosicionEnlazada(i,j);
+                Madera madera = new Madera();
                 madera.ingresar(mapa,posicion);
             }
         }
-
     }
 
     private void inicializarMapaConPiedra(){
         // ubico 7 piedras en el mapa
-        Piedra piedra = new Piedra();
         for (int i=2;i<4;i++) {
             for (int j = 10; j < 13; j++) {
                 PosicionEnlazada posicion = new PosicionEnlazada(i,j);
+                Piedra piedra = new Piedra();
                 piedra.ingresar(mapa,posicion);
             }
         }
         PosicionEnlazada posicion = new PosicionEnlazada(4,10);
+        Piedra piedra = new Piedra();
         piedra.ingresar(mapa,posicion);
 
     }
@@ -58,30 +63,40 @@ public class CreadorDeMapa {
 
     private void inicializarMapaConMetal(){
         // ubico 9 metales en el mapa
-        Metal metal = new Metal();
         for (int i=10;i<12;i++) {
             for (int j = 4; j < 9; j++) {
                 PosicionEnlazada posicion = new PosicionEnlazada(i,j);
+                Metal metal = new Metal();
                 metal.ingresar(mapa,posicion);
             }
         }
         PosicionEnlazada posicion = new PosicionEnlazada(12,4);
+        Metal metal = new Metal();
         metal.ingresar(mapa,posicion);
 
     }
 
     private void inicializarMapaConDiamante(){
-        // ubico 9 diamantes en el mapa
-        Diamante diamante = new Diamante();
-        for (int i=45;i<48;i++) {
-            for (int j = 50; j < 53; j++) {
+        // ubico 8 diamantes en el mapa
+        for (int i=13;i<15;i++) {
+            for (int j = 11; j < 15; j++) {
                 PosicionEnlazada posicion = new PosicionEnlazada(i,j);
+                Diamante diamante = new Diamante();
                 diamante.ingresar(mapa,posicion);
             }
         }
-        PosicionEnlazada posicion = new PosicionEnlazada(12,4);
-        diamante.ingresar(mapa,posicion);
 
+    }
+
+    private void agregarMaderasAdicionales(){
+        // ubico 4 maderas adicionales en el mapa
+        for (int i=7;i<9;i++) {
+            for (int j = 13; j < 15; j++) {
+                PosicionEnlazada posicion = new PosicionEnlazada(i,j);
+                Madera maderaAdicional = new Madera();
+                maderaAdicional.ingresar(mapa,posicion);
+            }
+        }
     }
 
     public Mapa obtenerMapaConRecursos(){
