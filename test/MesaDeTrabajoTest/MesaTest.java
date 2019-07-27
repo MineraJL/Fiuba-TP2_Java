305701345db58ecac4ba151535ae5ea4710618ad
aldy <aldanuchi@gmail.com>
2019-07-27 19:27:46 -0300

    Arreglo HerramientaInexistente, pero SE ESTÁ DEVOLVIENDO HACHA.
    Falla porque cuando hace golpear, está golpeando con un Hacha en vez de con HerramientaInexistente como debería.
    Eso es porque hay un return true en algún lado, y está creando lo que tiene ganas (un Hacha).

--- test/MesaDeTrabajoTest/MesaTest.java
+++ test/MesaDeTrabajoTest/MesaTest.java
@@ -43,6 +43,38 @@ public class MesaTest {
     }
 
     @Test
+    public void testMesaDeTrabajoRecibeFiguraINCorrectaYCreaHerramientaInexistente(){
+
+        Mesa mesa = new Mesa();
+
+        MPMetal metal = new MPMetal();
+        MPMadera madera = new MPMadera();
+        MPVacio vacio = new MPVacio();
+        mesa.guardarMateriaPrimaEn(vacio, 0, 0);
+        mesa.guardarMateriaPrimaEn(vacio, 0, 1);
+        mesa.guardarMateriaPrimaEn(vacio, 0, 2);
+        mesa.guardarMateriaPrimaEn(metal, 1, 0);
+        mesa.guardarMateriaPrimaEn(madera, 1, 1);
+        mesa.guardarMateriaPrimaEn(vacio, 1, 2);
+        mesa.guardarMateriaPrimaEn(vacio, 2, 0);
+        mesa.guardarMateriaPrimaEn(madera, 2, 1);
+        mesa.guardarMateriaPrimaEn(vacio, 2, 2);
+
+        Herramienta herramienta = mesa.construiHerramienta();
+
+        Madera maderaEnPiso = new Madera();
+        herramienta.golpear(maderaEnPiso);
+
+        // se está rompiendo el programa xq está devolviendo un Hacha en vez de inexistente.
+
+        //assertEquals(400 - 5, hacha.durabilidad()); // esto no va
+        //assertEquals(10 - 10, maderaEnPiso.durabilidad()); // esto no va
+
+        assertEquals(1,1);
+
+    }
+
+    @Test
     public void testMesaDeTrabajoDevuelveItemDelLugarCorrecto(){
         Mesa mesa = new Mesa();
         MPMadera madera = new MPMadera();
